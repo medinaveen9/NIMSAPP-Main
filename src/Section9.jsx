5e8f131120be200c634a8fbafd6b6798ab2d98f3
Naveen Kumar Medi <medinaveenkumar9@gmail.com>
2025-04-15 10:47:11 +0530

    Changes in Section1,section11,css,12,13,14,2,3,4,5,6,7,8,9

--- src/Section9.jsx
+++ src/Section9.jsx
@@ -1,109 +1,3 @@
-// import { useState } from "react";
-
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios";
-
-// const Section9 = () => {
-//   const [support_type, setSupportType] = useState("");
-//   const [additional, setAdditional] = useState("");
-//   const navigate = useNavigate();
-//   const Submit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const userResponse = await axios.post(
-//         "http://localhost:5001/additional_information",
-//         {
-//           support_type,
-//           additional,
-//         }
-//       );
-//       const id = userResponse.data.id;
-//       console.log("User created:", userResponse.data);
-//       navigate("/");
-//     } catch (error) {
-//       console.error(
-//         "Error:",
-//         error.response ? error.response.data : error.message
-//       );
-//     }
-//   };
-
-//   return (
-//     <div className="form-container">
-//       <form onSubmit={Submit}>
-//         <h5 className="h2">SECTION D: OTHER ISSUES </h5>
-//         <h4 className="h1">10.ADDITIONAL INFORMATION </h4>
-//         <div className="h">
-//           <h3 className="h">
-//             (a)Do you have any additional information to add in support of the
-//             application, which is not included elsewhere in the form? If yes,
-//             provide the details.{" "}
-//           </h3>
-//           <div className="radio-group">
-//             <label>
-//               <input
-//                 type="radio"
-//                 name="additional"
-//                 value="Yes"
-//                 checked={support_type === "Yes"}
-//                 onChange={(e) => setSupportType(e.target.value)}
-//               />{" "}
-//               Yes
-//             </label>
-//             <label>
-//               <input
-//                 type="radio"
-//                 name="additional"
-//                 value="No"
-//                 checked={support_type === "No"}
-//                 onChange={(e) => setSupportType(e.target.value)}
-//               />{" "}
-//               No
-//             </label>
-//           </div>
-//         </div>
-
-//         {support_type === "Yes" && (
-//           <div className="h">
-//             <h3>specify:</h3>
-//             <input
-//               type="text"
-//               name="additionalInformation"
-//               placeholder="Enter details"
-//               checked={additional === "Yes"}
-//               onChange={(e) => setAdditional(e.target.value)}
-//               className="custom-input"
-//               required
-//             />
-//             <br />
-//           </div>
-//         )}
-//         <button type="submit" className="custom-button">
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-// export default Section9;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -114,7 +8,7 @@ import axios from "axios";
 const Section9 = (adminId) => {
   const [support_type, setSupportType] = useState("");
   const [additional, setAdditional] = useState("");
-  const [preview, setPreview] = useState(false); // Preview mode toggle
+  const [preview, setPreview] = useState(false); 
 
   const navigate = useNavigate();
 
